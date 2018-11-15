0497432a114084a888f35c26ae20fe3fc91f3f47
bburns-ddog <benjamin.burns@datadoghq.com>
2018-11-15 08:10:18 -0800

    add other search options

--- migrate_monitors.py
+++ migrate_monitors.py
@@ -7,25 +7,42 @@ from datadog import initialize, api
 
 # source /opt/datadog/datadog-agent/venv/bin/activate
 
+# FROM org
+
 DD_API_KEY = os.getenv('DD_API_KEY', '') # from org api
 DD_APP_KEY = os.getenv('DD_APP_KEY', '') # from org app
 
+options = {
+    'api_key': DD_API_KEY,
+    'app_key': DD_APP_KEY
+}
+
+initialize(**options)
 good_keys = ['tags', 'deleted', 'query', 'message', 'matching_downtimes', 'multi', 'name', 'type', 'options']
 
-# replace this with your monitor query
-query_string = 'disk'
+# tags example
+search_string = 'env:system'
+search_key = 'tags' # common fields: name, query, tags, type
+
+# query example
+# search_string = 'disk'
+# search_key = 'query' # common fields: name, query, tags, type
 
 # get all monitors w/ matching query string
 monitors = api.Monitor.get_all()
 new_monitors = []
 for monitor in monitors:
     new_monitor = {}
-    q = monitor['query']
-    if query_string in q:
+    search_field = monitor[search_key]
+    if search_string == search_field or search_string in search_field:
         for k, v in monitor.items():
             if k in good_keys:
                 new_monitor[k] = v
         new_monitors.append(new_monitor)
+
+#pprint.pprint(new_monitors)
+
+# TO org
 
 options = {
     'api_key': 'new_api', # to org api
