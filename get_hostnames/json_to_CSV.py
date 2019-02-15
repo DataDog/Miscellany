eb53f75d63251492ce1a78691273eeca46bc8f3f
Anthony Lam <35714436+AyyLam@users.noreply.github.com>
2019-02-15 13:32:37 -0500

    Created json_to_CSV.py
    This file takes the JSON created from get_host_list.py and creates a CSV from it.

---
+++ get_hostnames/json_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import json
+import csv
+
+with open("host_all.json") as file:
+	jsondata = json.load(file)
+
+keys = jsondata[0].keys()
+
+#assert len(keys) == 2, Exception
+with open ("host_all.csv",'w') as outfile:
+	csv_file = csv.writer(outfile)
+	csv_file.writerow(keys)
+
+	for rows in jsondata:
+		ele = []
+		for key in keys:
+ 			ele.append(rows[key])		
+		csv_file.writerow(ele)
+print "complete converting json to csv"
