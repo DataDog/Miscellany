74f802e7efcd195ce8a72c10943b4aa925ccb603
ckelner <chris.kelner@gmail.com>
2019-04-23 17:02:42 -0700

    add/explain logic

--- all_dash_public/all-dash-public.py
+++ all_dash_public/all-dash-public.py
@@ -9,11 +9,37 @@ import json
 from halo import Halo
 
 def getAllPublicDashboards():
-    # @ckelner: These won't work - no way to get public token from API at this time
+    # @ckelner: These won't work - no way to get:
+        # - boolean as to whether the dashboard is public or not
+        # - public token from API at this time
+            # - If we could, then we could use the public dash API:
+                # - https://api.datadoghq.com/api/v1/public_dashboard/<token>
+            # - token generation is a combo of public org id and a randomly generated token
+                # def generate_token(cls, org_public_id):
+                #    '''
+                #    TOKEN_LENGTH size is set to 16 bytes.
+                #     - The random token size will be 32 chars
+                #     - The org_public_id is limited to 16 chars
+                #    So the full token size will be less than 49 chars
+                #    '''
+        # - public URL, or any indicator that the board is public
     # result = api.Dashboard.get_all()
     # result = api.Screenboard.get_all()
     # result = api.Screenboard.get(<redacted>)
     # result = api.Dashboard.get(<redacted>)
+    #
+    # Using various dashboard list API calls we can get this information though
+    # https://docs.datadoghq.com/api/?lang=python#dashboard-lists
+    #
+    # Logic:
+    # 1. Fetch all dashboard lists
+    # 2. Check that they have more than 1 dashboard in the list
+    # 3. Fetch each dashboard list item (the list itself doesn't contain the
+    # boolean we need to know if the dashboard is public or not)
+    # 4. Check is the dashboard is public or not
+    # 5. Check the dashboard id against a running dictionary we have to
+    # guarentee our dict is unique dashboards only
+    # 6. If unique place it in our dict to return to the user
     spinner = Halo(text="Getting all public dashboards; This might take awhile...", spinner="dots")
     spinner.start()
 
