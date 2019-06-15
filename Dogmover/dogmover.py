4d10e5f1cb8859bc4d7afa6b5152da81aa0da40f
Misiu Pajor <misiu.pajor@datadoghq.com>
2019-06-15 14:57:52 +0200

    Added support for 'dashboards', 'users' and 'synthetics'.

--- Dogmover/dogmover.py
+++ Dogmover/dogmover.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python2
 """Usage:
-  dogmover.py pull (<type>) [--dry-run]
-  dogmover.py push (<type>) [--dry-run]
+  dogmover.py pull (<type>) [--dry-run] [-h]
+  dogmover.py push (<type>) [--dry-run] [-h]
 
 Example, try with --dry-run first:
-    dogmover.py pull timeboards --dry-run
-    dogmover.py push timeboards --dry-run
+    dogmover.py pull dashboards --dry-run
+    dogmover.py push dashboards --dry-run
     to make actual changes:
-    dogmover.py pull timeboards
+    dogmover.py pull dashboards
     and finally push to another organization:
-    dogmover.py push timeboards
+    dogmover.py push dashboards
 
     Supported arguments:
-    dogmover.py pull|push screenboards|monitors|dashboards
+    dogmover.py pull|push dashboards|monitors|users|synthetics (--dry-run|-h)
 
 Options:
   -h, --help
   -d, --dry-run
 """
 __author__ = "Misiu Pajor <misiu.pajor@datadoghq.com>"
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 from docopt import docopt
 import json
 import os
 import glob
+import requests
 from datadog import initialize, api
 
 def _init_options(action):
@@ -67,39 +68,24 @@ def _files_to_json(type):
     files = glob.glob('{}/*.json'.format(type))
     return files
 
-
-def pull_screenboards():
-    path = "none, running in dry mode."
+def pull_dashboards():
+    path = False
     count = 0
-    screenboards = api.Screenboard.get_all()
 
-    for board in screenboards["screenboards"]:
-        count = count + 1 
-        json_data = api.Screenboard.get(board["id"])
-        if not arguments["--dry-run"]:
-            path = _json_to_file('screenboards', str(board["id"]), json_data)
-        print("Pulling: {} with id: {}, writing to file: {}".format(board["title"].encode('utf8'), board["id"], path))
-    print("Retrieved '{}' screenboards.".format(count))
-
-def pull_timeboards():
-    path = "none, running in dry mode."
-    count = 0
-    timeboards = api.Timeboard.get_all()
-
-    for board in timeboards["dashes"]:
+    dashboards = api.Dashboard.get_all()
+    for dashboard in dashboards["dashboards"]:
         count = count + 1
-        json_data = api.Timeboard.get(board["id"])
+        json_data = api.Dashboard.get(dashboard["id"])
         if not arguments["--dry-run"]:
-            path = _json_to_file('timeboards', str(board["id"]), json_data) 
-        print("Pulling: '{}' with id: {}, writing to file: {}".format(board["title"].encode('utf8'), board["id"], path))
-    print("Retrieved '{}' timeboards.".format(count))
-
+            path = _json_to_file('dashboards', dashboard["id"], json_data)
+        print("Pulling dashboard: {} with id: {}, writing to file: {}".format(dashboard["title"].encode('utf8'), dashboard["id"], path))
+    print("Retrieved '{}' dashboards.".format(count))
 
 def pull_monitors():
-    path = "none, running in dry mode."
+    path = False
+    count = 0
     good_keys = ['tags', 'deleted', 'query', 'message', 'matching_downtimes', 'multi', 'name', 'type', 'options', 'id']
     new_monitors = []
-    count = 0
 
     monitors = api.Monitor.get_all()
     for monitor in monitors:
@@ -110,72 +96,72 @@ def pull_monitors():
                 new_monitor[k] = v
         if not arguments["--dry-run"]:
             path = _json_to_file('monitors', str(new_monitor["id"]), new_monitor)
-        print("Pulling: {} with id: {}, writing to file: {}".format(new_monitor["name"].encode('utf8'), new_monitor["id"], path))
+        print("Pulling monitor: {} with id: {}, writing to file: {}".format(new_monitor["name"].encode('utf8'), new_monitor["id"], path))
     print("Retrieved '{}' monitors.".format(count))
-    return new_monitors
 
-
-def push_screenboards():
-    path = "none, running in dry mode."
+def pull_users():
+    path = False
     count = 0
 
-    screenboards = _files_to_json("screenboards")
-    if not screenboards:
-        exit("No screenboards have been pulled yet. Consider pulling screenboards first.")
-    for screenboard in screenboards:
-        with open(screenboard) as f:
-            data = json.load(f)
+    users = api.User.get_all()
+    for user in users["users"]:
+        if not user["disabled"]: # don't pull disabled users
             count = count + 1
-            if 'description' not in data:
-                data["description"] = ""
-            if 'width' not in data:
-                data["width"] = "1024"
-            # skip screenboards with no widgets in them
-            if 'widgets' not in data:
-                print("...Skipping {} as this screenboard has no widgets in it.".format(data["board_title"].encode("utf8")))
-                continue
-            print("Pushing {}".format(data["board_title"].encode('utf8')))    
+            json_data = api.User.get(user["handle"])
             if not arguments["--dry-run"]:
-                api.Screenboard.create(board_title=data["board_title"],
-                                    description=data["description"],
-                                    widgets=data["widgets"],
-                                    template_variables=data["template_variables"],
-                                    width=data["width"])
-            print("Pushed {} screenboards".format(count))
+                path = _json_to_file('users', user["handle"], json_data["user"])
+            print("Pulling user: {} with role: {}, writing to file: {}".format(user["handle"].encode('utf8'), user["access_role"], path))
+    print("Retrieved '{}' users.".format(count))    
 
-def push_timeboards():
-    path = "none, running in dry mode."
+def pull_synthetics(options):
+    path = False
     count = 0
 
-    timeboards = _files_to_json("timeboards")
-    if not timeboards:
-        exit("No timeboards have been pulled yet. Consider timeboards first.")
-    for timeboard in timeboards:
-        with open(timeboard) as f:
+    r = requests.get('{}api/v1/synthetics/tests?api_key={}&application_key={}'.format(options["api_host"], options["api_key"], options["app_key"]))
+    synthetics = r.json()
+    for synthetic in synthetics["tests"]:
+        if synthetic["type"] == "api": # skip browser checks for now as they do not emit the configuration in the JSON response
+            count = count + 1
+            json_data = requests.get('{}api/v1/synthetics/tests/{}?api_key={}&application_key={}'.format(
+                options["api_host"],
+                synthetic["public_id"],
+                options["api_key"],
+                options["app_key"]
+            )).json()
+            path = _json_to_file('synthetics', synthetic["public_id"], json_data)
+            print("Pulling: {} and writing to file: {}".format(synthetic["name"].encode('utf8'), path))
+    print("Retrieved '{}' synthetic tests.".format(count))  
+
+def push_dashboards():
+    count = 0
+    dashboards = _files_to_json("dashboards")
+    if not dashboards:
+        exit("No dashboards are locally available. Consider pulling dashboards first.")
+
+    for dashboard in dashboards:
+        with open(dashboard) as f:
             data = json.load(f)
             count = count + 1
-            if not 'template_variables' in data["dash"]:
-                data["dash"]["template_variables"] = ""
-            # skip timeboards with no graphs in them
-            if not data["dash"]["graphs"]:
-                print("...Skipping '{}' as this timeboard has no widgets in it.".format(data["dash"]["title"].encode('utf8')))
-                continue
-            print("Pushing: {}".format(data["dash"]["title"].encode('utf8')))
-            if not arguments["--dry-run"]:
-                api.Timeboard.create(title=data["dash"]["title"],
-                                    description=data["dash"]["description"],
-                                    graphs=data["dash"]["graphs"],
-                                    read_only=data["dash"]["read_only"])
-            print("Pushed {} timeboards.".format(count))
+            print("Pushing {}".format(data["title"].encode('utf8')))   
+            if not arguments["--dry-run"]:  
+                api.Dashboard.create(
+                    title=data["title"],
+                    description=data["description"],
+                    widgets=data["widgets"],
+                    template_variables=data["template_variables"],
+                    layout_type=data["layout_type"],
+                    notify_list=data["notify_list"],
+                    is_read_only=data["is_read_only"]
+                )
+    print("Pushed '{}' dashboards".format(count))
 
 
 def push_monitors():
-    path = "none, running in dry mode."
     count = 0
-
     monitors = _files_to_json("monitors")
     if not monitors:
-        exit("No monitors have been pulled yet. Consider pulling monitors first.")
+        exit("No monitors are locally available. Consider pulling monitors first.")
+
     for monitor in monitors:
         with open(monitor) as f:
             data = json.load(f)
@@ -188,31 +174,70 @@ def push_monitors():
                                     message=data['message'],
                                     tags=data['tags'],
                                     options=data['options'])
-    print("Pushed {} monitors.".format(count))
+    print("Pushed '{}' monitors.".format(count))
     if not arguments["--dry-run"]:
-        print("NOTE! All monitors have been automatically muted to suppress monitors from triggering false/positive alert. Please make sure to remove th scheduled downtime in the GUI if this is not what you wanted.")
+        print("Note. All monitors have been automatically muted to supress false/positive alerts. Navigate to Monitors -> Manage downtime to unmute.")
         api.Monitor.mute_all()
 
+def push_users():
+    count = 0
+    users = _files_to_json("users")
+    if not users:
+        exit("No users are locally available. Consider pulling users first.")
+
+    for user in users:
+        with open(user) as f:
+            data = json.load(f)
+            count = count + 1
+            print("Pushing: {}".format(data["handle"].encode('utf8')))
+            if not arguments["--dry-run"]:
+                api.User.create(
+                    handle=data["handle"],
+                    name=data["name"],
+                    access_role=data["access_role"]
+                )
+    print("Pushed '{}' users".format(count))
+
+def push_synthetics(options):
+    count = 0
+    synthetics = _files_to_json("synthetics")
+    if not synthetics:
+        exit("No synthetic tests are locally available. Consider synthetics first.")
+
+    for synthetic in synthetics:
+         with open(synthetic) as f:
+            data = json.load(f)
+            count = count + 1
+            invalid_keys = ["public_id", "monitor_id", "overall_state", "created_at", "created_by", "modified_by", "modified_at"]
+            list(map(data.pop, invalid_keys))
+            print("Pushing {}".format(data["name"].encode('utf8')))   
+            if not arguments["--dry-run"]:
+                r = requests.post('{}api/v1/synthetics/tests?api_key={}&application_key={}'.format(options["api_host"], options["api_key"], options["app_key"]), json=data)
+    print("Pushed '{}' synthetic tests.".format(count))
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.1.1rc')
 
     if arguments["--dry-run"]:
-        print("You are running in dry-mode. No actual pull/push will be done to the organizations.")
+        print("You are running in dry-mode. No changes will be commmited to your Datadog account(s).")
 
     if arguments["pull"]:
         _init_options("pull")
-        if arguments['<type>'] == 'screenboards':
-            pull_screenboards()
-        elif arguments['<type>'] == 'timeboards':
-            pull_timeboards()
+        if arguments['<type>'] == 'dashboards':
+            pull_dashboards()
         elif arguments['<type>'] == 'monitors':
             pull_monitors()
+        elif arguments['<type>'] == 'users':
+            pull_users()
+        elif arguments['<type>'] == 'synthetics':
+            pull_synthetics(_init_options("pull"))
     elif arguments["push"]:
         _init_options("push")
-        if arguments['<type>'] == 'screenboards':
-            push_screenboards()
-        elif arguments['<type>'] == 'timeboards':
-            push_timeboards()
+        if arguments['<type>'] == 'dashboards':
+            push_dashboards()
         elif arguments['<type>'] == 'monitors':
             push_monitors()
+        elif arguments['<type>'] == 'users':
+            push_users()
+        elif arguments['<type>'] == 'synthetics':
+            push_synthetics(_init_options("push"))
