cfc537a5593a70ab4212d5d52f3f1b0a233c55f2
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2019-07-11 14:15:02 -0400

    Create migrate dashboard script
    Created a dashboard migration script, as the timeboard and screenboard scripts are outdated since we changed our API endpoints. The dashboard migration script will work for both timeboards and screenboards.

---
+++ Dashboards/migrate_dashboard.py
@@ -0,0 +1,34 @@
+from datadog import initialize, api
+
+options = {
+    'api_key': '<YOUR_OLD_ACCT_API_KEY>',
+    'app_key': '<YOUR_OLD_ACCT_APP_KEY>'
+}
+
+dashboard_id = '<DASHBOARD_ID>'
+
+initialize(**options)
+
+dashboard = api.Dashboard.get(dashboard_id)
+
+print(dashboard)
+
+
+options = {
+    'api_key': '<YOUR_NEW_ACCT_API_KEY>',
+    'app_key': '<YOUR_NEW_ACCT_APP_KEY>'
+}
+
+initialize(**options)
+
+new = api.Dashboard.create(
+	title=dashboard['title'],
+	widgets=dashboard['widgets'],
+    layout_type=dashboard['layout_type'],
+    description=dashboard['description'],
+    is_read_only=dashboard['is_read_only'],
+    notify_list=dashboard['notify_list'],
+	template_variables=dashboard['template_variables'],
+)
+
+print(new)
