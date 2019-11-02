d41a7cee68268d9faad12cb3a91120e858557ecf
darthnater007 <nsperk@gmail.com>
2019-11-02 13:49:13 -0400

    clean spacing in outputs, update readme, tests successful

--- metrics_on_dashboards_report/metrics_on_dashboards_report.py
+++ metrics_on_dashboards_report/metrics_on_dashboards_report.py
@@ -28,7 +28,7 @@ def get_metric_report(ids_list, metrics_to_eval):
 
                     #build the dashboards with metrics string for each board, adding title of the board and metric name
                     if board_title != resp["title"]:
-                        print('\n\tBoard: ' + resp["title"])
+                        print('\n\n\tBoard: ' + resp["title"])
                     print('\n\t\t Metric: ' + metric)
 
 #user input logic flow
@@ -39,7 +39,7 @@ while choice != 'y':
 
     #get api and app keys from user
 
-    api_key = input ('This program is designed to return a list of Datadog Dashboards that contain specific metrics. \nTo get started, please enter your API Key:\n')
+    api_key = input ('This program is designed to return a list of Datadog Dashboards that contain specific metrics. \n\nTo get started, please enter your API Key:\n')
     print('\n')
     app_key = input ('Thank you.  Please enter a valid app_key\n')
     print('\n')
@@ -49,10 +49,10 @@ while choice != 'y':
     while not is_number:
         try:
             num_of_metrics_to_search = input('How many metrics would you like to search for?\n')
-            print('\n')
+            print('\n\n')
             is_number = True
         except NameError:
-            print('Whoops!  This entry must be an integer.  Please try again. \n')
+            print('Whoops!  This entry must be an integer.  Please try again. \n\n')
             
     #declare empty list to populate with metrics
     metrics_to_eval = []
@@ -61,37 +61,40 @@ while choice != 'y':
     for i in range(0,int(num_of_metrics_to_search)):
         metric = input('Enter metric #' + str(i+1)+': ')
         metrics_to_eval.append(metric)
+    
+    print("\n--------")
 
-    choice = input('api_key: ' + api_key + '\napp_key: ' + app_key + '\nnumber of metrics to search: ' + str(num_of_metrics_to_search) + '\nlist of metrics to search for: ' + str(metrics_to_eval) + '\nIf this is correct enter \'y\' to continue, or enter any other key to start over: ')
-    print('\n')
+    choice = input('\napi_key: ' + api_key + '\n\napp_key: ' + app_key + '\n\nnumber of metrics to search: ' + str(num_of_metrics_to_search) + '\n\nlist of metrics to search for: ' + str(metrics_to_eval) + '\n\nIf this is correct enter \'y\' to continue, or enter any other key to start over: ')
+    print('\n\n')
 #run it!
 
 #initialize app and api keys for making API calls
-print('Intializing API...\n')
+print('Intializing API...\n\n')
 options = {
     'api_key' : api_key,
     'app_key' : app_key
 }
 
 initialize(**options)
+
 #Test to see that we're properly initialized by making a call to the API that requires no arguments
 test_resp = api.DashboardList.get_all()
 if test_resp.get('errors') is None:
-    print('Intialized!\n')
+    print('API Intialized!\n\n')
 else:
     print('There was a problem Initialiizing the API.  Please restart and check your API and App Keys for validity.')
     quit()
 
 #return every dashboard
-print('Getting All Dashboards\n')
+print('Getting All Dashboards\n\n')
 resp = api.Dashboard.get_all()
-print('All Dashboards Received!\n')
+print('All Dashboards Received!\n\n')
 
 #get lists of ids from every dashboard
-print('Getting Dashboard Id\'s\n')
+print('Getting Dashboard Id\'s\n\n')
 id_list = get_all_dashboard_id_list(resp)
-print("ID\'s received")
+print("ID\'s received\n\n")
 
 #format and output to the console
-print('Getting Your Report\n')
+print('Getting Your Report\n\n')
 get_metric_report(id_list, metrics_to_eval)
