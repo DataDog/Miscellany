da43778ffe1a8cd696bbc6e1f8c6dc9457954328
gsalami00 <g.a.salamat@gmail.com>
2020-01-06 13:14:33 -0500

    added search minimum characters and added backup to dashboards

--- delete_dashboards_by_text_in_title/README.md
+++ delete_dashboards_by_text_in_title/README.md
@@ -1,6 +1,6 @@
 # Delete Dashboards by Text within the Title
 
-This script lets you delete dashboards based on text within the title or the title itself. For example, if you have dashboards named 'Dashboard One' and 'Dashboard Two', if you enter 'Dashboard' in the prompt, this will allow for the deletion of both dashboards. 
+This script lets you delete dashboards based on their title.
 
 ## Steps
 
@@ -13,7 +13,5 @@ pip install datadog
 ```
 python delete_dashboards.py
 ```
-This will prompt you to enter your API key, application key, and the text for the dashboard you want to delete, and will ask for confirmation before deleting the dashboards found.
 
-## Note
-The text you enter for the dashboards to delete is case-sensitive.
+This will prompt you to enter your API key and application key (if they are not stored as an environment variable—`DD_API_KEY` and `DD_APP_KEY`), and the text to search for the dashboard(s) you want to delete. It will ask for confirmation before deleting the dashboards found and will also create a backup dashboard file named 'datadog_dashboards_backup.txt.'
--- delete_dashboards_by_text_in_title/delete_dashboards.py
+++ delete_dashboards_by_text_in_title/delete_dashboards.py
@@ -1,44 +1,92 @@
+import os
 from datadog import initialize, api
 
 
-def delete_dashboards_by_text_in_title():
+def file_output(dashboard, title):
+    if os.path.exists("datadog_dashboards_backup.txt"):
+        mode = "a"
+    else:
+        mode = "w"
 
-    api_key = input("Please enter your API key: ")
-    app_key = input("Please enter your APPLICATION key: ")
+    print(f"Writing backup for {title} on file datadog_dashboards_backup.txt.")
+    with open("datadog_dashboards_backup.txt", mode) as f:
+        f.write(f"Dashboard Title: {title}\n\n{dashboard}\n\n\n")
+
+
+def get_search_text():
+    while True:
+        text = input(
+            "Please enter the text to search in your dashboard titles: "
+        ).strip()
+        if len(text) >= 3:
+            return text
+        print("Search text must be at least 3 characters!")
+
+
+def confirm_delete(dash_id, dash_title):
+    confirm = input(f'Delete dashboard entitled "{dash_title}"? [Y/n]: ')
+    if confirm.lower() == "y":
+        try:
+            api.Dashboard.delete(dash_id)
+            print(f'Dashboard entitled "{dash_title}" was deleted.')
+        except Exception as e:
+            print(e)
+    else:
+        print(f'Dashboard entitled "{dash_title}" was NOT deleted.')
+
+
+def initialize_options():
+    if "DD_API_KEY" in os.environ:
+        api_key = os.getenv("DD_API_KEY")
+    else:
+        api_key = input("Please enter your API key: ")
+
+    if "DD_APP_KEY" in os.environ:
+        app_key = os.getenv("DD_APP_KEY")
+    else:
+        app_key = input("Please enter your APPLICATION key: ")
 
     options = {"api_key": api_key, "app_key": app_key}
 
     initialize(**options)
 
-    text_to_search_in_dashboard = input(
-        "Please enter the text to search in your dashboard titles: "
-    )
 
-    dashboards = api.Dashboard.get_all()["dashboards"]
+def delete_dashboards_by_text_in_title():
 
-    dashboards_to_delete = {}
+    initialize_options()
 
-    for dashboard in dashboards:
-        if text_to_search_in_dashboard in dashboard["title"]:
-            dashboards_to_delete[dashboard["id"]] = dashboard["title"]
+    search_text = get_search_text()
 
-    dashboard_list = dashboards_to_delete.values()
+    try:
+        dashboards = api.Dashboard.get_all()
+    except:
+        raise RuntimeError("Your API or APPLICATION key may be invalid.")
 
-    if len(dashboard_list) == 0:
+    to_delete = {}
+
+    for d in dashboards["dashboards"]:
+        if search_text.lower() in d["title"].lower():
+            to_delete[d["id"]] = d["title"]
+            result = api.Dashboard.get(d["id"])
+            file_output(result, d["title"])
+
+    if not to_delete:
         print("There were no dashboards found for this query.")
     else:
-        print("Dashboards to delete:\n" + "\n".join(dashboard_list))
+        print("Dashboards to delete:")
+        for i, dashboard in enumerate(to_delete.values()):
+            print(f"  {i + 1}. {dashboard}")
 
         delete_or_not = input(
             "These dashboards will be deleted. Do you want to continue? [Y/n]: "
         )
 
         if delete_or_not.lower() == "y":
-            for dash_id in dashboards_to_delete.keys():
-                api.Dashboard.delete(dash_id)
-            print("The dashboards in the list were deleted.")
+            for dash_id, dash_title in to_delete.items():
+                confirm_delete(dash_id, dash_title)
+            print("Dashboard deletion completed.")
         else:
-            print("No dashboards were deleted.")
+            print("Dashboard deletion aborted.")
 
 
 if __name__ == "__main__":
