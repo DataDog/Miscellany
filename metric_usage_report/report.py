1cfd4b677d19297ff2ee3b3bc97c3980e848c84a
Nicholas Devlin <nicholas.devlin@datadoghq.com>
2021-05-01 16:04:20 -0400

    cleanup script
    move widget logic into a function
    
    replace counter with enumeration

--- metric_usage_report/report.py
+++ metric_usage_report/report.py
@@ -8,40 +8,34 @@ import sys
 import json
 import os
 
-def find_widgets(dashboard, report):
+def search_widget(dashboard, widget, report):
+    if str(widget["definition"].get("requests")).find(metric) >= 0:
+        # initialize dashboard entry
+        if dashboard["id"] not in report[metric]:
+            report[metric][dashboard["id"]] = {"title":dashboard.get("title"),
+                "url":dashboard.get("url"),
+                "author":dashboard.get("author_handle"),
+                "widgets":{}}
+
+        # add widget to list
+        report[metric][dashboard["id"]]["widgets"][widget["id"]] = widget["definition"].get("title","")
+
+
+def search_dashboard(dashboard, report):
     if "widgets" not in dashboard:
-        print(dashboard)
+        print("No widgets object found in dashboard\n", dashboard)
         return
     for widget in dashboard.get("widgets"):
 
-        # if a provided metric is present in a widget, save it
         for metric in config.METRICS_TO_EVAL:
 
-            # loop through group widgets
+            # nested loop through group widgets
             if widget["definition"].get("type") == "group":
                 for child_widget in widget["definition"]["widgets"]:
-                    if widget["definition"].get("requests") and str(widget["definition"]["requests"]).find(metric) >= 0:
-                    # initialize dashboard entry
-                        if dashboard["id"] not in report[metric]:
-                            report[metric][dashboard["id"]] = {"title":dashboard["title"],
-                                "url":dashboard["url"],
-                                "author":dashboard["author_handle"],
-                                "widgets":{}}
-
-                        # add widget to list
-                        report[metric][dashboard["id"]]["widgets"][widget["id"]] = widget["definition"].get("title","")
+                    search_widget(dashboard, child_widget, report)
                 break
-
-            if str(widget["definition"].get("requests")).find(metric) >= 0:
-                # initialize dashboard entry
-                if dashboard["id"] not in report[metric]:
-                    report[metric][dashboard["id"]] = {"title":dashboard.get("title"),
-                        "url":dashboard.get("url"),
-                        "author":dashboard.get("author_handle"),
-                        "widgets":{}}
-
-                # add widget to list
-                report[metric][dashboard["id"]]["widgets"][widget["id"]] = widget["definition"].get("title","")
+            
+            search_widget(dashboard, widget, report)
 
 
 # Print iterations progress
@@ -67,6 +61,7 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_lengt
         sys.stdout.write('\n')
     sys.stdout.flush()
 
+
 if __name__ == "__main__":
     report_init.init()
 
@@ -76,19 +71,16 @@ if __name__ == "__main__":
     report = {}
     for metric in config.METRICS_TO_EVAL:
         report[metric] = {}
-    
-    i = 0
 
     if os.path.isfile(config.DB_CACHE_PATH):
         print("Cache file found, loading dashboard date from", config.DB_CACHE_PATH)
         with open(config.DB_CACHE_PATH, "r") as file:
             db_count = file.readline()
             print("db_count:",db_count)
-            for line in file:
-                find_widgets(json.loads(line), report)
+            for i, line in enumerate(file):
+                search_dashboard(json.loads(line), report)
 
-                i += 1
-                print_progress(i, db_count, bar_length = 50)
+                print_progress(i+1, db_count, bar_length = 50)
 
     else:
         print("No cache file found, writing API results to", config.DB_CACHE_PATH)
@@ -98,17 +90,17 @@ if __name__ == "__main__":
             db_count = len(db_ids)
 
             file.write(str(db_count) + "\n")
-            for db_id in db_ids:
+            for i, db_id in enumerate(db_ids):
                 db_response = api.Dashboard.get(db_id)
-                find_widgets(db_response, report)
+                if "errors" not in db_response:
+                    search_dashboard(db_response, report)
 
-                i += 1
-                print_progress(i, db_count, bar_length = 50)
+                    file.write(json.dumps(db_response))
+                    file.write("\n") # concatenating strings is expensive, writing a newline to a file is not
 
-                file.write(json.dumps(db_response))
-                file.write("\n") # concatenating strings is expensive, writing a newline to a file is not
-    print("\n")
-    print(report)
+                    print_progress(i+1, db_count, bar_length = 50)
+
+    print("\n", report)
 
 
     # print("\n***MONITORS***\n")
