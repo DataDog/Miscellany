f72210ab628fd00e90bc5bad3e30f4d4a9b17398
Ian Bucad <ian.bucad@gmail.com>
2023-11-13 11:58:15 +1100

    Update remove_lingering_aws_host_tags.py
    - add support for sites; defaults to US1
    - tag_source now case-insensitive

--- remove_lingering_aws_host_tags.py
+++ remove_lingering_aws_host_tags.py
@@ -10,22 +10,33 @@ import json
 # 3rd Party
 import requests
 
-
 # Input Variables
 API_KEY = 'YOUR_API_KEY_HERE'
 APPLICATION_KEY = 'YOUR_APPLICATION_KEY_HERE'
 TAG_SOURCE = 'Amazon Web Services'  # Can also be set to 'Users', 'Chef', or
                                     # or other source values.
+                                    # See : https://docs.datadoghq.com/integrations/faq/list-of-api-source-attribute-value/
+SITE = 'US1' # US1, US3, US5, EU1, US1-FED, AP1. 
+             # See: https://docs.datadoghq.com/getting_started/site/ 
 REMOVE_FROM_ALL_HOSTS = False  # change to true to remove from all hosts
                                # be careful not to remove tags you want to keep!
 HOSTS = [  # if not all, specify what hosts you want to delete tags from
     'host_name'
 ]
 
+# constants
+SITES = {
+    'US1': 'datadoghq.com',
+    'US3': 'us3.datadoghq.com',
+    'US5': 'us5.datadoghq.com',
+    'EU1': 'datadoghq.eu',
+    'US1-FED': 'ddog-gov.com',
+    'AP1': 'ap1.datadoghq.com',
+}
 
 # functions
 def remove_host_tags_by_source(hostname, source, session):
-    remove_endpt = 'https://app.datadoghq.com/api/v1/tags/hosts/' + hostname
+    remove_endpt = 'https://app.{}/api/v1/tags/hosts/{}'.format(SITES.get(SITE, SITES['US1']), hostname)
     session.params['source'] = source
     res = session.request(
         method='DELETE', url=remove_endpt, params=session.params
@@ -42,15 +53,18 @@ s.params = {
 }
 
 if REMOVE_FROM_ALL_HOSTS is True:
-    perma_link = 'https://app.datadoghq.com/reports/v2/overview'
+    perma_link = 'https://app.{}/reports/v2/overview'.format(SITES.get(SITE, SITES['US1']))
     perma_content = s.request(
         method='GET', url=perma_link, params=s.params
     ).text
     perma_json = json.loads(perma_content)
-    
-    for host in perma_json['rows']:
+
+    # if 'errors' in perma_json:
+    #     print(perma_json)
+    # else:
+    for host in perma_json.get('rows', []):
         host_name = host['host_name']
-        if TAG_SOURCE in host['tags_by_source']:
+        if TAG_SOURCE.lower() in set(s.lower() for s in host['tags_by_source']):
             remove_host_tags_by_source(host_name, TAG_SOURCE, s)
         else:
             print("no %s tags in %s, skipping." % (TAG_SOURCE, host_name))
