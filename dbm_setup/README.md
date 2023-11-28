a60fe434195a56750fc97beb704179b6ff63077e
UTXOnly <brian.hartford@datadoghq.com>
2023-11-28 11:59:35 -0500

    Add dbm setup script

---
+++ dbm_setup/README.md
@@ -0,0 +1,66 @@
+# Postgres Database Management Setup Script 
+
+## Overview
+
+The DBM setup script is a Python script designed to facilitate preparing your Postgres databses for monitoring with [Datadog's Database Monitoring](https://docs.datadoghq.com/database_monitoring/setup_postgres/selfhosted/?tab=postgres15), including user and schema creation, extension installation, and checking PostgreSQL statistics. This document provides instructions on how to use the setup script to prepare your databases. 
+
+## Prerequisites
+
+Before using the script, ensure the following prerequisites are met:
+
+- Python 3.10 is installed on your system
+- The database user used to connect to the Postgres instance has admin access
+- Your Postgres databse has `pg_stat_statements` enabled in your `postgresql.conf` file
+```
+shared_preload_libraries = 'pg_stat_statements'  # (change requires restart)
+```
+
+## Setup
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/yourusername/dbm2.git
+   cd dbm2
+
+2. Edit the `.env`` in the project directory and add the following environment variables with your PostgreSQL connection details:
+   ```
+   DBHOST=your_database_host
+   PORT=your_database_port
+   DBNAME=your_database_name
+   DBUSER=your_database_user
+   PASSWORD=your_database_password
+   ```
+
+3. Run the Script:
+   * This command activates the virtual environment, installs/upgrades dependencies, and executes the `dbm_setup.py` script.
+
+
+   ```
+   make run
+   ```
+
+### Review Output:
+The script will output information about the progress of tasks, such as user creation, schema creation, and PostgreSQL statistics checks.
+
+#### Testing the script
+You can test the script on a docker container to see how this works before running on your database.
+
+To run a Postgres Docker container for testing, runt he docker command below from repo root directory:
+```
+docker run -d --name postgres-container -p 5432:5432 \              
+  -e POSTGRES_PASSWORD=postpass \
+  -v $(pwd)/postgresql.conf:/etc/postgresql/postgresql.conf \
+  postgres -c 'config_file=/etc/postgresql/postgresql.conf'
+```
+
+To create an additional databse for testing:
+```
+psql -h localhost -p 6333 -U postgres -c "CREATE DATABASE testdb;"
+```
+
+![Alt text](<./img/Screen Recording 2023-11-28 at 10.11.06 AM.gif>)
+
+### Check PostgreSQL Statistics:
+After running the script, review the console output to ensure that PostgreSQL connections, activity, and statistics are reported correctly. If no errors are present, the script will print a list of databses that were successfully prepared at the end of the script.
+
+![Alt text](<./img/Image 2023-11-28 at 11.03.44 AM.jpg>)
---
+++ dbm_setup/dbm_setup.py
@@ -0,0 +1,160 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+GREEN = "\033[0;32m"
+RED = "\033[0;31m"
+RESET = "\033[0m"
+
+def print_error(message):
+    print(f"{RED}{message}{RESET}")
+
+def print_success(message):
+    print(f"{GREEN}{message}{RESET}")
+
+def get_connection_params():
+    return {
+        'host': os.getenv('DBHOST'),
+        'port': os.getenv('PORT'),
+        'dbname': os.getenv('DBNAME'),
+        'user': os.getenv('DBUSER'),
+        'password': os.getenv('PASSWORD')
+    }
+
+def create_datadog_user_and_schema(conn, db):
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1 FROM pg_roles WHERE rolname='datadog'")
+            exists = cur.fetchone()
+            if not exists:
+                cur.execute("CREATE USER datadog WITH PASSWORD 'datadog'")
+                print_success(f"datadog user created in {db} database")
+                conn.commit()
+
+        with conn.cursor() as cur:
+            cur.execute("SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'datadog')")
+            schema_exists = cur.fetchone()[0]
+    except Exception as exc:
+        print_error(f"An error occurred while querying for schema {exc}")
+
+    if schema_exists:
+        print_error(f"datadog schema already exists in {db} database")
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute("CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
+    except Exception as e:
+        print_error(f"An error occurred while executing CREATE EXTENSION: {e}")
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute("CREATE SCHEMA datadog; GRANT USAGE ON SCHEMA datadog TO datadog; GRANT USAGE ON SCHEMA public TO datadog; GRANT pg_monitor TO datadog;")
+            print_success(f"datadog schema created and permissions granted in {db} database")
+            conn.commit()
+    except Exception as e:
+        print_error(f"An error occurred while creating datadog schema and granting permissions: {e}")
+
+def explain_statement(conn):
+    try:
+        with conn.cursor() as cur:
+            cur.execute("""
+            CREATE OR REPLACE FUNCTION datadog.explain_statement(
+                l_query TEXT,
+                OUT explain JSON
+            )
+            RETURNS SETOF JSON AS
+            $$
+            DECLARE
+                curs REFCURSOR;
+                plan JSON;
+            BEGIN
+                OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+                FETCH curs INTO plan;
+                CLOSE curs;
+                RETURN QUERY SELECT plan;
+            END;
+            $$
+            LANGUAGE 'plpgsql'
+            RETURNS NULL ON NULL INPUT
+            SECURITY DEFINER;
+            """)
+            #conn.commit()
+            #time.sleep(2)
+        print_success("Explain plans statement completed")
+    except Exception as exc:
+        print_error(f"Error encountered creating explain statement: {exc}")
+
+
+def create_pg_stat_statements_extension(conn_obj):
+    try:
+        with conn_obj.cursor() as cur:
+            # Check if pg_stat_statements is already installed
+            cur.execute("SELECT 1 FROM pg_extension WHERE extname = 'pg_stat_statements';")
+            result = cur.fetchone()
+
+            if not result:
+                # If not installed, then install the extension
+                cur.execute("CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
+                print(f"{GREEN}pg_stat_statements extension installed successfully")
+            else:
+                print(f"{GREEN}pg_stat_statements extension already installed")
+    except psycopg2.Error as e:
+        print(f"{RED}Error installing pg_stat_statements extension: {e}")
+
+successful_setup = []
+
+def check_postgres_stats(conn_obj, db):
+    try:
+        # Create pg_stat_statements extension if not already created
+        create_pg_stat_statements_extension(conn_obj)
+
+        with conn_obj.cursor() as cur:
+            cur.execute("SELECT 1 FROM pg_stat_database LIMIT 1;")
+            print(f"{GREEN}Postgres connection - OK in {db}")
+
+            cur.execute("SELECT 1 FROM pg_stat_activity LIMIT 1;")
+            print(f"{GREEN}Postgres pg_stat_activity read OK in {db}")
+
+            cur.execute("SELECT 1 FROM pg_stat_statements LIMIT 1;")
+            print(f"{GREEN}Postgres pg_stat_statements read OK in {db}")
+        successful_setup.append(db_name)
+        print(f"{RED}\n############### Moving On... to next database ###############################\n{RESET}")
+        
+    except psycopg2.OperationalError as exc:
+        print(f"{RED}Error querying pg_stats from{db}{RESET}: {exc}")
+    except psycopg2.Error:
+        print(f"{RED}Error while accessing Postgres statistics in {db}{RESET}")
+
+def list_databases(conn):
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT datname FROM pg_database WHERE datistemplate = false")
+            databases = [row[0] for row in cur.fetchall() if not row[0].startswith('template')]
+    except Exception as exc:
+        print_error(f"Error encountered listing databases: {exc}")
+
+    return databases
+
+try:
+    connection_params = get_connection_params()
+    conn = psycopg2.connect(**connection_params)
+    databases_list = list_databases(conn)
+except psycopg2.Error as e:
+    print_error(f"An error occurred while connecting to the database: {e}")
+
+
+# Iterate through the list of database names, run checks, and create schemas
+for db_name in databases_list:
+    print_success(f"Discovered database: {db_name}\nCreating schema and checking permissions + stats")
+    connection_params['dbname'] = db_name
+    conn = psycopg2.connect(**connection_params)
+    create_datadog_user_and_schema(conn, db_name)
+    explain_statement(conn)
+    check_postgres_stats(conn, db_name)
+
+
+print("Setup complete!")
+print_success("The databse monitoring setup completed sucessfully on the following databses:")
+print(f"\n {successful_setup}")
